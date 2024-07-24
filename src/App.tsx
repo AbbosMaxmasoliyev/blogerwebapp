1067684650c0219517391f7f7cc7b2d1740af036
AbbosMaxmasoliyev <otajonmaxmasoliyev775@gmail.com>
2024-07-24 17:15:43 +0500

    Updated vercel json

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Promotions from './pages/Promotions';
 import Navbar from './components/navbar';
