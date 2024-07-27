51011dff212305a648632ea6fbec84543b02d6e8
AbbosMaxmasoliyev <otajonmaxmasoliyev775@gmail.com>
2024-07-27 14:26:58 +0500

    Added MONGO

--- src/services/config.tsx
+++ src/services/config.tsx
@@ -1,5 +1,6 @@
 import axios from "axios"
 export const API_PREFIX: string = "https://bloggerbot.uz"
+// export const API_PREFIX: string = "http://localhost:3001"
 
 const BaseService = axios.create({
     baseURL: API_PREFIX,
