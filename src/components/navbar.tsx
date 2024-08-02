0eed108bdd9d97031729530516c68c934a869634
AbbosMaxmasoliyev <otajonmaxmasoliyev775@gmail.com>
2024-08-02 13:56:02 +0500

    Pushed test for userfirstname

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -99,7 +99,7 @@ const Navbar: React.FC = () => {
                                             <span
                                                 className="text-gray-800 dark:text-white font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:ring-0 focus:outline-none"
                                             >
-                                                {user ? user.firstName : t("loading")}
+                                                {user ? user?.firstName : t("loading")}
                                             </span>
                                             {/* <img
                                                 alt="user avatar"
