302a01c766b76b33a4fddaf03d6a6f2eb685a61f
AbbosMaxmasoliyev <otajonmaxmasoliyev775@gmail.com>
2024-08-03 17:07:45 +0500

    Pushed changed 47

--- src/pages/PromotionAgree.tsx
+++ src/pages/PromotionAgree.tsx
@@ -64,7 +64,11 @@ const PromotionAgreeWith: React.FC<PromotionAgreeWithProps> = ({ promotion, user
                         {/* <p className="inline-flex font-medium items-center text-blue-600 hover:underline">
                             $ {promotion.price}
                         </p> */}
-                        <button type="button" className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700 flex gap-2 items-center mt-3" onClick={handleAgreePromotion} >{t("agree")} <FiSend /></button>
+
+                        {
+                            userId != promotion.owner?.userId ?
+                                <button type="button" className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700 flex gap-2 items-center mt-3" onClick={handleAgreePromotion} >{t("agree")} <FiSend /></button> : null
+                        }
                     </div>
 
                     {
