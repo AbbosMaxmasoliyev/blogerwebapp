1d5dfc51484aeff889e65a89545c9f5148b7b7be
AbbosMaxmasoliyev <otajonmaxmasoliyev775@gmail.com>
2024-08-07 15:46:16 +0500

    Pushed test for promotion

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -171,18 +171,9 @@ const Navbar: React.FC = () => {
                                                 role="menuitem"
                                                 className="flex w-full cursor-pointer select-none items-center gap-2 rounded-md px-3 pt-[9px] pb-2 text-start leading-tight outline-none transition-all hover:bg-blue-gray-50 hover:bg-opacity-80 hover:text-blue-gray-900 focus:bg-blue-gray-50 focus:bg-opacity-80 focus:text-blue-gray-900 active:bg-blue-gray-50 active:bg-opacity-80 active:text-blue-gray-900"
                                             >
-                                                <svg
-                                                    xmlns="http://www.w3.org/2000/svg"
-                                                    fill="none"
-                                                    viewBox="0 0 24 24"
-                                                    stroke-width="2"
-                                                    stroke="currentColor"
-                                                    aria-hidden="true"
-                                                    className="h-4 w-4"
-                                                >
-                                                    <GrDocumentUser />
 
-                                                </svg>
+                                                <GrDocumentUser />
+
                                                 <p className="block font-sans text-sm font-normal leading-normal text-inherit antialiased">
                                                     {t("my_promotions")}
                                                 </p>
