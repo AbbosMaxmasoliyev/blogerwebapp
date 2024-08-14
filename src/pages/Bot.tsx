ce51aff404e4df3087dd5ca69cb39dce3e531dbb
AbbosMaxmasoliyev <otajonmaxmasoliyev775@gmail.com>
2024-08-14 11:52:24 +0500

    Pushed for category multiselect13

--- src/pages/Bot.tsx
+++ src/pages/Bot.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, useEffect, useRef, useState } from 'react';
+import { ChangeEvent, useEffect, useState } from 'react';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
 import { FaYoutube, FaInstagram, FaTelegram } from 'react-icons/fa';
